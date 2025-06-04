1cbfe8ebdb203114c2b439dabe58161adfbc2aa4
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-06-04 10:43:51 +0700

    Create 523. Continuous Subarray Sum.cpp

---
+++ 523. Continuous Subarray Sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        mp[0] = -1;
+        int ans= 0;
+        for(int i = 0 ; i < nums.size();i++){
+            ans = (ans + nums[i]) % k;
+            if(mp.count(ans)){
+                if(i - mp[ans] > 1){
+                return true;
+               }
+            }
+            else mp[ans] = i;
+        }
+        return false;
+    }
+};
